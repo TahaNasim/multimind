d5263d0592e87181e4b2f550aa3214e4f79ea7f1
TahaNasim <tahanasimxic33@gmail.com>
2025-10-17 14:51:46 +0530

    save my local changes before pulling

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -244,16 +244,7 @@ return (
             {loading ? 'Loading...' : isForgotPassword ? 'Send Reset Link' : isLogin ? 'Sign In' : 'Sign Up'}
           </button>
         </form>
-         <ModelPreferencesModal
-      open={showModelModal}
-      selected={selectedModels}
-      onChange={setSelectedModels}
-      onClose={() => setShowModelModal(false)}
-      onSave={handleSavePreferences}
-    />
-
-
-        <div className="mt-6 text-center space-y-3">
+         <div className="mt-6 text-center space-y-3">
           {isForgotPassword ? (
             <button
               onClick={() => {
